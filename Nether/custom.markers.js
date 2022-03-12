479fb30bff381306326ef919dbc92c624ecd260a
Piper <piperdeck@gmail.com>
2022-03-12 04:16:01 -0500

    Sat 12 Mar 04:16:01 EST 2022 -- automated update

--- Nether/custom.markers.js
+++ Nether/custom.markers.js
@@ -38,46 +38,59 @@ Use the imageScale property if the pin image is too large.
 
 UnminedCustomMarkers = {
 
-    isEnabled: false,
+    isEnabled: true,
 
     markers: [
 
-        // Example 1: Simple marker
+        // // Example 1: Simple marker
+        // {
+        //     x: -200,
+        //     z: -200,
+        //     image: "custom.pin.png",
+        //     imageAnchor: [0.5, 1],
+        //     imageScale: 0.5,
+        // },
+
+        // // Example 2: Marker with text
+        // {
+        //     x: 0,
+        //     z: 0,
+        //     image: "custom.pin.png",
+        //     imageAnchor: [0.5, 1],
+        //     imageScale: 0.5,
+        //     text: "Marker with text",
+        //     textColor: "red", 
+        //     offsetX: 0,
+        //     offsetY: 20,
+        //     font: "bold 20px Calibri,sans serif",
+        // },
+
+        // // Example 3: Text only
+        // {
+        //     x: 200,
+        //     z: 200,
+        //     text: "Text only", 
+        //     textColor: "yellow", 
+        //     offsetX: 0,
+        //     offsetY: 0, 
+        //     font: "bold 50px Calibri,sans serif",
+        // },
+
+
+        //// Nether Gate 
         {
-            x: -200,
-            z: -200,
+            x: 20,
+            z: 31,
             image: "custom.pin.png",
             imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-        },
-
-        // Example 2: Marker with text
-        {
-            x: 0,
-            z: 0,
-            image: "custom.pin.png",
-            imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-            text: "Marker with text",
-            textColor: "red", 
+            imageScale: 0.3,
+            text: "Nether Gate",
+            textColor: "white",
+            strokeColor: "#000000", 
             offsetX: 0,
-            offsetY: 20,
-            font: "bold 20px Calibri,sans serif",
+            offsetY: 15,
+            font: "bold 15px Georgia,serif",
         },
-
-        // Example 3: Text only
-        {
-            x: 200,
-            z: 200,
-            text: "Text only", 
-            textColor: "yellow", 
-            offsetX: 0,
-            offsetY: 0, 
-            font: "bold 50px Calibri,sans serif",
-        },
-
-        // add your markers here
-
 
 
 
